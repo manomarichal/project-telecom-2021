155c85ae5f52724c392755b546759c8b51533331
manomarichal <mano.marichal@student.uantwerpen.be>
2021-04-08 14:17:20 +0200

    changed data types

--- elements/local/igmp/igmp.h
+++ elements/local/igmp/igmp.h
@@ -13,19 +13,20 @@
 #include <click/ipaddress.hh>
 
 // struct used to represent a single group record, see RFC3367 page 14
-struct igmp_group_record
+struct igmp_group_record_message
 {
     uint8_t record_type; // see RFC3376 page 16
-    const uint8_t aux_data_len = 0; //IGMPv3 does not define any auxiliary data thus length must be zero
+    const uint8_t aux_data_len = 0; // IGMPv3 does not define any auxiliary data thus length must be zero
     uint16_t number_of_sources;
     uint32_t multicast_adress;
-    Vector<uint32_t> source_adresses;
+    Vector<uint32_t> source_adresses; // maybe use arrays here to avoid sending extra ptrs
 };
 
 // struct used to represent a membership report, see RFC3367 page 13
+WriteablePacket *
 struct igmp_membership_report
 {
-    uint8_t type;
+    const uint8_t type = 0x22; // indicate its a membership record
     const uint8_t reserved_1 = 0;
     uint16_t number_of_group_records;
     uint16_t checksum;
