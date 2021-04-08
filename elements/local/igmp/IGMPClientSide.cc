a464edc20b8590f0bb330ea576cb29128567770d
robbe <vandevelde.robbe@gmail.com>
2021-04-08 11:26:37 +0200

    added comments and client_leave handler

--- elements/local/igmp/IGMPClientSide.cc
+++ elements/local/igmp/IGMPClientSide.cc
@@ -12,17 +12,34 @@ IGMPClientSide::~IGMPClientSide()
 
 int IGMPClientSide::configure(Vector<String> &conf, ErrorHandler *errh)
 {
-    return 0;
-}
+    if (Args(conf, this, errh).read_m("MAXPACKETSIZE", maxSize).complete() < 0) return -1;
+    if (maxSize <= 0) return errh->error("maxsize should be larger than 0");
+    return 0;}
 
+/*
+ * handles the client join for the system.
+ * return integer, done throughout other click elements
+ */
 int IGMPClientSide::client_join(const String &conf, Element *e, void *thunk, ErrorHandler *errh) {
-    click_chatter("hallo");
+    click_chatter("hallo, join");
     return 0;
 }
 
+int IGMPClientSide::client_leave(const String &conf, Element *e, void *thunk, ErrorHandler *errh) {
+    click_chatter("hallo, leave");
+    return 0;
+}
+
+/*
+ * function which initialises the handlers, current handlers contain:
+ * -client_join which will let the client join the reception of a given multicast address /rfc page 4
+ * -client leave which wil let the client leave the reception of a given multicast address /rfc page 4
+ */
 void IGMPClientSide::add_handlers() {
-    add_write_handler("client_join", &client_join, (void *)0);
-//    add_write_handler("client_leave")
+    click_chatter("hallo, adding handlers");
+
+    add_write_handler("client_join", &client_join, 0);
+    add_write_handler("client_leave", &client_leave, 0);
 }
 
 CLICK_ENDDECLS
