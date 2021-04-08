4c3816468cbaa4a51674a9b41151c69e3667bf9b
manomarichal <mano.marichal@student.uantwerpen.be>
2021-04-08 13:52:32 +0200

    fixed unknown type

--- elements/local/igmp/igmp.h
+++ elements/local/igmp/igmp.h
@@ -12,15 +12,24 @@
 
 #include <click/ipaddress.hh>
 
-// struct used to represent a single group record for a client, see RFC3367 page 14
-
+// struct used to represent a single group record, see RFC3367 page 14
 struct igmp_group_record
 {
-    uint32_t record_type; // see RFC3376 page 16
-    const uint32_t aux_data_len = 0; //IGMPv3 does not define any auxiliary data thus lenght must be zero
-    uint32_t number_of_sources;
-    IPAddress multicast_adress;
+    uint8_t record_type; // see RFC3376 page 16
+    const uint8_t aux_data_len = 0; //IGMPv3 does not define any auxiliary data thus length must be zero
+    uint16_t number_of_sources;
+    uint32_t multicast_adress;
     Vector<IPAddress> source_adresses;
+};
+
+// struct used to represent a membership report, see RFC3367 page 13
+struct igmp_membership_report
+{
+    uint8_t type;
+    const uint8_t reserved_1 = 0;
+    uint16_t number_of_group_records;
+    uint16_t checksum;
+    const uint16_t reserved1 = 0;
 };
 
 #endif //PROJECT_TELECOM_IGMP_H
