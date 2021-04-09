03f7ee315b29f5ccfe0a6a6db7f09276f99e4a45
manomarichal <mano.marichal@student.uantwerpen.be>
2021-04-09 12:04:59 +0200

    client test

--- bash_scripts/vm_test_client_element.sh
+++ bash_scripts/vm_test_client_element.sh
@@ -1,20 +1,19 @@
 set -e
-echo "emptying igmp folder"
+echo "================= REMOVING OLD FILES ===================="
 rm -rf /home/student/click/elements/local/igmp
 mkdir /home/student/click/elements/local/igmp
-echo "copying new files"
+echo "================= COPYING NEW FILES ===================="
 cp -r /home/student/Desktop/project-telecom-2021/elements/local/igmp /home/student/click/elements/local/igmp
-echo "updating reference start.sh"
+echo "================= UPDATING START SCRIPTS ===================="
 rm -f /home/student/click/scripts/start_click.sh
 cp /home/student/Desktop/project-telecom-2021/igmp_opgave/scripts/start_scripts/reference_scripts/start_script_client_reference.sh /home/student/click/scripts/start_click.sh
-echo "updating student start.sh"
 rm -f /home/student/click-reference/solution/start_click.sh
 cp /home/student/Desktop/project-telecom-2021/igmp_opgave/scripts/start_scripts/student_scripts/start_script_client_student.sh /home/student/click-reference/solution/start_click.sh
-echo "building click"
+echo "================= BUILDING CLICK ===================="
 cd /home/student/click
 make elemlist
 make
-echo "running click"
+echo "================= RUNNING CLICK ===================="
 chmod +x /home/student/click-reference/solution/start_click.sh
 chmod +x /home/student/click/scripts/start_click.sh
 sudo /home/student/click-reference/solution/start_click.sh
