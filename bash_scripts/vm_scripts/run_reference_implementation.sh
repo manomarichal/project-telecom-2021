b0f5453604320bb3b26a0df9197fc190febcf855
manomarichal <mano.marichal@student.uantwerpen.be>
2021-04-11 14:54:12 +0200

    updated referene solutoin script

--- bash_scripts/vm_scripts/run_reference_implementation.sh
+++ bash_scripts/vm_scripts/run_reference_implementation.sh
@@ -1,4 +1,8 @@
 set -e
+echo "================= RESTORING REFERENCE FILES ===================="
+sudo rm -rf /home/student/click/scripts/library
+mkdir /home/student/click/scripts/library
+cp -r /home/student/Desktop/project-telecom-2021/igmp_opgave/scripts/library /home/student/click/scripts
 echo "================= UPDATING START SCRIPTS ===================="
 rm -f /home/student/click/scripts/start_click.sh
 cp /home/student/Desktop/project-telecom-2021/bash_scripts/start_scripts/reference_scripts/reference.sh /home/student/click/scripts/start_click.sh
@@ -6,4 +10,3 @@ rm -f /home/student/click-reference/solution/start_click.sh
 cp /home/student/Desktop/project-telecom-2021/bash_scripts/start_scripts/student_scripts/student.sh /home/student/click-reference/solution/start_click.sh
 echo "================= RUNNING CLICK ===================="
 chmod +x /home/student/click-reference/solution/start_click.sh
-/home/student/click-reference/solution/start_click.sh
---
+++ bash_scripts/vm_scripts/setup.sh
@@ -0,0 +1,6 @@
+set -e
+sudo /home/student/click/scripts/setup.sh
+chmod +x ./bash_scripts/vm_scripts/run_reference_implementation.sh
+chmod +x ./bash_scripts/vm_scripts/test_client.sh
+chmod +x ./bash_scripts/vm_scripts/test_router+client.sh
+chmod +x ./bash_scripts/vm_scripts/test_router.sh
