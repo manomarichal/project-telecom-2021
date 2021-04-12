9fa6fecdccf63514ba8f67612794da2e168b4f89
mano <mano.marichal@gmail.com>
2021-04-12 11:43:47 +0200

    small push uwu

--- bash_scripts/vm_scripts/test_client.sh
+++ bash_scripts/vm_scripts/test_client.sh
@@ -8,10 +8,10 @@ echo "================= COPYING NEW FILES ===================="
 cp -r /home/student/Desktop/project-telecom-2021/elements/local/igmp /home/student/click/elements/local/igmp
 cp -r /home/student/Desktop/project-telecom-2021/library /home/student/click/scripts
 echo "================= UPDATING START SCRIPTS ===================="
-rm -f /home/student/click/scripts/start_click.sh
-cp /home/student/Desktop/project-telecom-2021/bash_scripts/start_scripts/reference_scripts/client_reference.sh /home/student/click/scripts/start_click.sh
 rm -f /home/student/click-reference/solution/start_click.sh
-cp /home/student/Desktop/project-telecom-2021/bash_scripts/start_scripts/student_scripts/client_student.sh /home/student/click-reference/solution/start_click.sh
+cp /home/student/Desktop/project-telecom-2021/bash_scripts/start_scripts/reference_scripts/client_reference.sh /home/student/click-reference/solution/start_click.sh
+rm -f /home/student/click/scripts/start_click.sh
+cp /home/student/Desktop/project-telecom-2021/bash_scripts/start_scripts/student_scripts/client_student.sh /home/student/click/scripts/start_click.sh
 echo "================= BUILDING CLICK ===================="
 cd /home/student/click
 make elemlist
