c92a60452faf4ca09bff9a8bbf6e4fadfd574385
student <vandevelde.robbe@gmail.com>
2021-04-12 14:01:43 +0200

    added basis for routerside element

--- elements/local/igmp/IGMPRouterSide.cc
+++ elements/local/igmp/IGMPRouterSide.cc
@@ -1,5 +1,29 @@
 //
 // Created by Gebruiker on 7/04/2021.
 //
-
+#include <click/config.h>
+#include <click/args.hh>
+#include <click/error.hh>
+#include <click/ipaddress.hh>
 #include "IGMPRouterSide.hh"
+
+
+CLICK_DECLS
+IGMPRouterSide::IGMPRouterSide()
+{}
+
+IGMPRouterSide::~IGMPRouterSide()
+{}
+
+/*
+ * configure for routerside object, only takes therouterip or interface as input
+ */
+int IGMPRouterSide::configure(Vector<String> &conf, ErrorHandler *errh)
+{
+    if (Args(conf, this, errh).read_mp("ROUTERADDRESS", routerIP).complete()<0){click_chatter("failed read when initialising router, returning 0"); return -1;}
+    click_chatter("initialising routeraddress %s", routerIP.unparse().c_str());
+    return 0;
+}
+
+CLICK_ENDDECLS
+EXPORT_ELEMENT(IGMPRouterSide) // forces to create element within click
