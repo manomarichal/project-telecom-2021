6323fa493e0d6b9b5fc47c1c9fd0ff208576f525
student <vandevelde.robbe@gmail.com>
2021-04-13 14:57:38 +0200

    updated join test

--- bash_scripts/vm_scripts/join_test.sh
+++ bash_scripts/vm_scripts/join_test.sh
@@ -2,4 +2,8 @@ echo "connecting to localhost 10003 and executing join on 244.4.4.4"
 
 echo "write client21/igmpclient.client_join 244.4.4.4" | telnet localhost 10003
 
+sleep 20
+
+echo "write client21/igmpclient.client_leave 244.4.4.4" | telnet localhost 10003
+
 
