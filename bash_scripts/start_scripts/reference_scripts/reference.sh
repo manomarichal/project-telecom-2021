b19d0e7dbd72ee79bae266647894266abe5f31e7
mano <mano.marichal@gmail.com>
2021-04-13 15:00:09 +0200

    corrected reference solution script

--- bash_scripts/start_scripts/reference_scripts/reference.sh
+++ bash_scripts/start_scripts/reference_scripts/reference.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
+cd /home/student/click/scripts/
+../userlevel/click glue.click &
 cd /home/student/click-reference/solution/
-
-/home/student/click/userlevel/click /home/student/click/scripts/glue.click &
 
 sleep 1
 
-./router.bin &   # port 10001
-./client21.bin & # port 10003
-./client22.bin & # port 10004
-./client31.bin & # port 10005
-./client32.bin & # port 10006
-./server.bin &   # port 10002
+./router.bin &
+./server.bin &
+./client21.bin &
+./client22.bin &
+./client31.bin &
+./client32.bin &
 
 wait
