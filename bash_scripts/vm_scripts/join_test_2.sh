de5066c8a746049703caa95711aaed8fdb1899aa
student <vandevelde.robbe@gmail.com>
2021-04-16 11:17:22 +0200

    changed syntax of handlers

--- bash_scripts/vm_scripts/join_test_2.sh
+++ bash_scripts/vm_scripts/join_test_2.sh
@@ -1,13 +1,13 @@
 echo "connecting to localhost 10003 and executing join on 225.1.1.1"
 
-echo "write client21/igmpclient.client_join 225.1.1.1" | telnet localhost 10003
+echo "write client21/igmp.join 225.1.1.1" | telnet localhost 10003
 
 sleep 5
 
-echo "write client21/igmpclient.client_join 225.1.1.1" | telnet localhost 10003
+echo "write client21/igmp.join 225.1.1.1" | telnet localhost 10003
 
 sleep 5
 
-echo "write client21/igmpclient.client_leave 225.1.1.1" | telnet localhost 10003
+echo "write client21/igmp.leave 225.1.1.1" | telnet localhost 10003
 
 
