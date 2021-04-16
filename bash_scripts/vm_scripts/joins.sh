da9bb8aedf78311c478a4cb59510ab9543fbe748
Robbe Van de Velde <33662570+robbevandevelde@users.noreply.github.com>
2021-04-16 11:19:04 +0200

    Update joins.sh

--- bash_scripts/vm_scripts/joins.sh
+++ bash_scripts/vm_scripts/joins.sh
@@ -1,21 +1,23 @@
-echo "connecting to localhost 10003 and executing join on 225.1.1.1 (client21)"
+echo "======JOINS======"
 
-echo "write client21/igmpclient.client_join 225.1.1.1" | telnet localhost 10003
+echo "==connecting to localhost 10003 and executing join on 225.1.1.1 (client21)=="
 
-sleep 5
-
-echo "connecting to localhost 10004 and executing join on 225.1.1.1 (client22)"
-
-echo "write client22/igmpclient.client_join 225.1.1.1" | telnet localhost 10004
+echo "write client21/igmp.join 225.1.1.1" | telnet localhost 10003
 
 sleep 5
 
-echo "connecting to localhost 10005 and executing join on 225.1.1.1 (client31)"
+echo "==connecting to localhost 10004 and executing join on 225.1.1.1 (client22)=="
 
-echo "write client31/igmpclient.client_join 225.1.1.1" | telnet localhost 10005
+echo "write client22/igmp.join 225.1.1.1" | telnet localhost 10004
 
 sleep 5
 
-echo "connecting to localhost 10006 and executing join on 225.1.1.1 (client32)"
+echo "==connecting to localhost 10005 and executing join on 225.1.1.1 (client31)=="
 
-echo "write client32/igmpclient.client_join 225.1.1.1" | telnet localhost 10006
+echo "write client31/igmp.join 225.1.1.1" | telnet localhost 10005
+
+sleep 5
+
+echo "==connecting to localhost 10006 and executing join on 225.1.1.1 (client32)=="
+
+echo "write client32/igmp.join 225.1.1.1" | telnet localhost 10006
